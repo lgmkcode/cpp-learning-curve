277531419ab10172a2281e9aa5701f3b1177f19c
lgmk.code <halileee601@gmail.com>
2023-07-04 09:54:35 +0300

    add basic example

---
+++ 2-language-basics/example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "Enter a first number:";
+    cin >> a;
+    cout << "Enter a second number:";
+    cin >> b;
+
+    cout << "a+b=" << a+b << endl;
+    cout << "a-b=" << a-b << endl;
+    cout << "a*b=" << a*b << endl;
+    cout << "a/b=" << (float)a/b << endl;
+    cout << "a%b=" << a%b << endl;
+ return 0;
+}
