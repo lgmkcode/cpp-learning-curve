21d0c0e64011fc4493408e33d0832e8c3b1dd435
lgmk.code <halileee601@gmail.com>
2023-07-04 08:56:38 +0300

    add input output

---
+++ 2-language-basics/2.3-input-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cout << "Enter a number:"; //output
+    cin >> a; //input
+    cout << "square of the entered number:" << a*a << endl;
+
+    return 0;
+
+
+}
