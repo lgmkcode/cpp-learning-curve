33da54f15fcac5cf7b55c154f401a2df862d2be5
lgmk.code <halileee601@gmail.com>
2023-07-06 11:21:46 +0300

    add for

---
+++ 4-loops/for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    for(int i = 0; i<5 ; i++){  //we can define "i" outside if we want
+
+        cout << "for loop" << i << endl;
+
+    }
+
+return 0;
+
+}
