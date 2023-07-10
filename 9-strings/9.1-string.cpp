e0b0ca570e867d3450197b00cae66d7ce91b301e
lgmk.code <halileee601@gmail.com>
2023-07-10 14:13:38 +0300

    update string

--- 9-strings/9.1-string.cpp
+++ 9-strings/9.1-string.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main(){
@@ -17,5 +18,16 @@ a[2]= 'r';
 a[3]= '\0'; // must be defined for a correct output
 cout << a << endl;
 
+// strcmp --> string compare
+// returns 0 if they equal
+// returns -1 if first string is small
+// returns 1 if the second string is small
+
+if(strcmp(c,s)==0){
+    cout << "equal" << endl;
+}
+else{
+    cout << "not equal" << endl;
+}
 return 0;
 }
